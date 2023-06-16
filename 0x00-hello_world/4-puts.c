4e7e56acaaa07c20faaf550465b5b8c54abfbb2b
RefilweM1 <RefilweMokwene908@gmail.com>
2023-06-15 17:27:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Entry prints "Programming is like building a multilingual 
+ * puzzle, followed by a newline'
+ * Return: Always 0.
  */
+
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
