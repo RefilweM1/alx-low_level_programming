cb0faa3bb0aea67e96b3861e8efd71868aa46fbd
RefilweM1 <RefilweMokwene908@gmail.com>
2023-06-20 10:58:19 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,9 @@
 void print_alphabet_x10(void)
 {
 	char num, letter;
-        for (num = 0; num <= 9; num++)
+        for (num = 0; num <= 9; num++);
         {
-		for (letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++);
 		{
 		_putchar(letter);
 		}
