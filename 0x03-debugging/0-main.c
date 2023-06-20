16c2ff2ee19c9161be958e857f78f7cf1a3b7f72
RefilweM1 <RefilweMokwene908@gmail.com>
2023-06-20 04:28:50 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - testsfunction that prints if integer is positive or negative
+ * Retuen: 0
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative (1);
+	return (0);
+}
