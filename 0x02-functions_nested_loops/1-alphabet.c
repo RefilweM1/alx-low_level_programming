88d841c56a431a9d2efb43fba3599153a823871e
RefilweM1 <RefilweMokwene908@gmail.com>
2023-06-21 17:24:48 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,12 @@
  * print_alphabet - Entry point
  * Return: 0
  */
-
 void print_alphabet(void)
 {
 int(i);
-
 for (i = 97; i <= 122; i++)
 {
 _putchar(i);
 }
-_putchar('\n');
-	
+_putchar('\n');	
 }
