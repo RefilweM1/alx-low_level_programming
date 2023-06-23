cbbc5e45a61f955d7985685fe06b47caed55e8c2
RefilweM1 <RefilweMokwene908@gmail.com>
2023-06-22 17:31:09 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,13 +1,12 @@
-#include "main,h"
 #include <unistd.h>
+
 /**
- * _putchar - writes the character c to stdout
- * @c: The charcter to print
- *
- * Return: on success 1
- * On error, -1 is returned, and erno is set apropriately
- */
+*_putchar - write the character c to stdout
+*@c:The character to print
+*Return 1 if true, 0 if false
+*On error, -1 return.
+*/
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
