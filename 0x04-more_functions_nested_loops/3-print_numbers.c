01edd914496ab5843ce5bd8d7823ab75560e7194
RefilweM1 <RefilweMokwene908@gmail.com>
2023-06-23 18:02:31 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * prin numbers - Print the numbers from 0 up to 9
  * Return: The number from 0 up to 9
@@ -7,10 +8,10 @@
 
 void print_numbers(void)
 {
-	int x;
-	for(x = 0; x <= 9; x++)
-	{
-		putchar(x + '0');
-	}
-	putchar('\n');
+int x;
+for(x = 0; x <= 9; x++)
+{
+putchar(x + '0');
+}
+putchar('\n');
 }
