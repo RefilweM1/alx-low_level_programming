cd5896f61f217897ffa027c003917062cc2f56ab
RefilweM1 <RefilweMokwene908@gmail.com>
2023-07-10 03:44:08 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *array = malloc(size);
 		if (size == 0 || array == 0)
 			return (NULL);
-	while (size --)
+	while (size--)
 		array[size] = c;
 	return (array);
 }
