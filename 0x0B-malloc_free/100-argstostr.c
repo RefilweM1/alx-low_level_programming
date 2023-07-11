29cca4868cf01f6022bad525c5948d1ede535505
RefilweM1 <RefilweMokwene908@gmail.com>
2023-07-11 08:26:26 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,32 +1,40 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * *argstostr - concatenates all the arguments ofhte progrm
- * @ac: nmber of arguments
- * @av: array of arguments
- * Return: Pointer to the new string (success), NULL (error)
- */
+* *argstostr - concatenates all the arguments of the program
+* @ac: number of arguments
+* @av: array of arguments
+*
+* Return: Pointer to the new string (Success), NULL (Error)
+*/
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, len;
-	char *str;
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-	for (j = 0; av[i][j] != '\0'; j++)
-		len++;
-	len++;
-	}
-	str = malloc(sizeof(char) * (len + 1));
-	if (str == NULL)
-		return (NULL);
-	(k = 0);
-	for (j = 0; av[i][j] != '\0'; j++)
-	{
-		str[k] = av[i][j];
-		k++;
-	}
-	return (str);
+int i, j, k, len;
+char *str;
+
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+len++;
+len++;
+}
+str = malloc(sizeof(char) * (len + 1));
+if (str == NULL)
+return (NULL);
+k = 0;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+str[k] = av[i][j];
+k++;
+}
+str[k] = '\n';
+k++;
+}
+return (str);
 }
