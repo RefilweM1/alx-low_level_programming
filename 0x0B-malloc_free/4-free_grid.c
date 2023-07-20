bb34ab709f456d9a9dec6fec5c1636bd2ce53f12
RefilweM1 <RefilweMokwene908@gmail.com>
2023-07-20 12:00:51 -0700

    0-sum_them_all.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,41 +2,17 @@
 #include <stdlib.h>
 
 /**
- * argstostr - main entry
- * @ac: int input
- * @av: double pointer array
- * Return: 0
+ * free_grid - frees the memory allocated for the grid
+ * created by alloc_grid()
+ * @grid: grid to free
+ * @height: height of the grid
  */
-
-char *argstostr(int ac, char **av)
+void free_grid(int **grid, int height)
 {
-int i, n, r = 0, l = 0;
-char *str;
-
-if (ac == 0 || av == NULL)
-return (NULL);
-
-for (i = 0; i < ac; i++)
-{
-for (n = 0; av[i][n]; n++)
-l++;
-}
-l += ac;
-
-str = malloc(sizeof(char) * l + 1);
-if (str == NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
-{
-for (n = 0; av[i][n]; n++)
-{
-str[r] = av[i][n];
-r++;
-}
-if (str[r] == '\0')
-{
-str[r++] = '\n';
-}
-}
-return (str);
+	int i;
+	if (grid == NULL ||height == 0)
+		return;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }
