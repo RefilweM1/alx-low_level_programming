45097b928da0b1417a95eeebd0f877739d79820f
RefilweM1 <RefilweMokwene908@gmail.com>
2023-08-01 07:35:32 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
  * free_listint_safe-frees a linked list
- * @h:pointer to the first node of the linked list
- * Return:number of elements in the freed lsit
+ * @h:pointer to the first node in the linked list
+ * Return:number of elements in the freed list
  */
 size_t free_listint_safe(listint_t **h)
 {
@@ -10,7 +10,7 @@ size_t free_listint_safe(listint_t **h)
 	int diff;
 	listint_t *temp;
 
-	if (!h || *h)
+	if (!h || !*h)
 
 		return (0);
 
@@ -33,5 +33,6 @@ size_t free_listint_safe(listint_t **h)
 		}
 	}
 	*h = NULL;
+
 	return (len);
 }
