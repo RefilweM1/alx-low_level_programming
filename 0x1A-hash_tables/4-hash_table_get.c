a7f26338d185be274abc453cf093ffef9c2acc99
RefilweM1 <RefilweMokwene908@gmail.com>
2023-10-06 04:38:40 -0700

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - get value from the hash table
+ * @ht:hash tables
+ * @key:key
+ * Return:the value if it works, NULL if it doesn't
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current_node;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	index = key_index ((const unsigned char *) key, ht->size);
+	if ((ht->array)[index] == NULL)
+		return (NULL);
+	current_node = (ht->array)[index];
+	while (current_node)
+	{
+		if (strcmp(current_node->key, key) == 0)
+			return (current_node->value);
+		current_node = current_node->next;
+	}
+	return (NULL);
+}
